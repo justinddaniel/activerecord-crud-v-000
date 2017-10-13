9da8ef128666c40e54b126713610183c050cb781
Justin Daniel <justinddaniel@gmail.com>
2017-10-13 16:56:59 +0000

    automatically backed up by learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -6,7 +6,7 @@
 # end                              # end
 
 def can_be_instantiated_and_then_saved
-  movie = Movie.new
+  movie = Movie.new(attributes: nil)
   movie.title = "This is a title."
   movie.save
 end
