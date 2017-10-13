3dc8257d25edb5b981638b0fb89151891c6336e9
Justin Daniel <justinddaniel@gmail.com>
2017-10-13 16:48:34 +0000

    automatically backed up by learn

--- db/migrate/20171013164141_create_movies.rb
+++ db/migrate/20171013164141_create_movies.rb
@@ -1,6 +1,6 @@
 class CreateMovies < ActiveRecord::Migration
   def change
-    create_table movies do |m|
+    create_table :movies do |m|
       m.string :title
       m.integer :release_date
       m.string :director
